9da58403de742385ba597800d522f79ab3bb09da
Laif-takaya <prayingrun1001@gmail.com>
2019-10-16 21:30:26 +0900

    TEST3

--- sousa.py
+++ sousa.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import datetime
 from time import sleep
+
 # 1.操作するブラウザを開く
 #WebDriverを格納しているディレクトリを指定
 def highLow(date):
@@ -18,9 +19,9 @@ def highLow(date):
         #driverに設定 ※optionsを指定しないとheadlessにならないので注意
         driver = webdriver.Chrome(options=options, executable_path=driver_path)
         #driver = webdriver.Chrome()
+        driver.set_window_size(1280, 720)
         # 2.操作するページを開く
         driver.get('https://demotrade.highlow.com/')
-        driver.set_window_size(1280, 720)
         # 基本設定はここまで。↑は使い回し可能。ここから下は、やりたい動作によって増える
         sleep(2)
         # 3.操作する要素を指定
