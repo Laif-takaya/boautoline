8b09abcb65b2efc4b024a2219d29a16ba8b9e437
Laif-takaya <prayingrun1001@gmail.com>
2019-10-16 12:47:16 +0900

    test2

--- sousa.py
+++ sousa.py
@@ -1,9 +1,5 @@
 # PythonでSeleniumのwebdriverモジュールをインポート
-import re
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import datetime
-from time import sleep
+
 # 1.操作するブラウザを開く
 #WebDriverを格納しているディレクトリを指定
 def highLow(date):
