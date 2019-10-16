0ded8ec53c7ccac67678837a1f4291cd15d6493a
Laif-takaya <prayingrun1001@gmail.com>
2019-10-16 20:06:55 +0900

    TEST2

--- sousa.py
+++ sousa.py
@@ -11,15 +11,16 @@ def highLow(date):
     stop=date[0:2]
     up_down=date[-2:]
     try:
-        options = Options()
-        options.binary_location = '/app/.apt/usr/bin/google-chrome'
+        # chromedriverのPATHを指定(herokuにおけるパスを指定しています)
+        driver_path = '/app/.chromedriver/bin/chromedriver'
+        options = webdriver.ChromeOptions()
         options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(chrome_options=options)
+        #driverに設定 ※optionsを指定しないとheadlessにならないので注意
+        driver = webdriver.Chrome(options=options, executable_path=driver_path)
         #driver = webdriver.Chrome()
-        driver.set_window_size(1280, 720)
         # 2.操作するページを開く
         driver.get('https://demotrade.highlow.com/')
+        driver.set_window_size(1280, 720)
         # 基本設定はここまで。↑は使い回し可能。ここから下は、やりたい動作によって増える
         sleep(2)
         # 3.操作する要素を指定
