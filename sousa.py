380da48ef2109e577e586db73b714f7e39a9fb53
Laif-takaya <prayingrun1001@gmail.com>
2019-10-16 19:21:59 +0900

    更新

--- sousa.py
+++ sousa.py
@@ -1,12 +1,15 @@
 # PythonでSeleniumのwebdriverモジュールをインポート
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+import datetime
+from time import sleep
 # 1.操作するブラウザを開く
 #WebDriverを格納しているディレクトリを指定
 def highLow(date):
     currency_pair=date[:7]
     stop=date[0:2]
     up_down=date[-2:]
-    result ="er"
+    result ="errow"
+    
     return result
