5bc83b765c6ccf3061ec86e0040305ccd06a4f12
Laif-takaya <prayingrun1001@gmail.com>
2019-10-16 12:42:17 +0900

    test1

--- sousa.py
+++ sousa.py
@@ -10,22 +10,5 @@ def highLow(date):
     currency_pair=date[:7]
     stop=date[0:2]
     up_down=date[-2:]
-    try:
-        options = Options()
-        options.binary_location = '/app/.apt/usr/bin/google-chrome'
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(chrome_options=options)
-        #driver = webdriver.Chrome()
-        driver.set_window_size(1280, 720)
-        # 2.操作するページを開く
-        driver.get('https://demotrade.highlow.com/')
-        # 基本設定はここまで。↑は使い回し可能。ここから下は、やりたい動作によって増える
-
-        driver.quit()
-        result ="OK"
-        return result
-    except:
-        result ="errow"
-        driver.quit()
-        return result
+    result ="OK"
+    return result
