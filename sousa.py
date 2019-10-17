19eeb152364adce1bfa0644b0bac53548e64791f
Laif-takaya <prayingrun1001@gmail.com>
2019-10-17 09:04:20 +0900

    check3

--- sousa.py
+++ sousa.py
@@ -36,7 +36,7 @@ def highLow(date):
         if ab>=200000:
             ab=200000
 
-        result =currency_pair+up_down
+        result =currency_pair
         driver.quit()
 
         return result
