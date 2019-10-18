c1c433501b4277404f8260c1fe7c51a4757aa40e
Laif-takaya <prayingrun1001@gmail.com>
2019-10-18 18:42:44 +0900

    ch6

--- sousa.py
+++ sousa.py
@@ -17,14 +17,13 @@ def highLow(date):
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
-        options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36')
         #driverに設定 ※optionsを指定しないとheadlessにならないので注意
         driver = webdriver.Chrome(options=options, executable_path=driver_path)
         driver.set_window_size(1280, 720)
         # 2.操作するページを開く
         driver.get('https://demotrade.highlow.com/')
         # 基本設定はここまで。↑は使い回し可能。ここから下は、やりたい動作によって増える
-        sleep(2)
+        sleep(10)
         # 3.操作する要素を指定
         # 4.その要素を操作する
         driver.find_element_by_xpath('//*[@id="header"]/div/div/div/div/div/span/span/a[1]/i').click()
