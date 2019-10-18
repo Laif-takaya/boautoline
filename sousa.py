de123b1913c400833203eb66b8fc9319a28dfe7e
Laif-takaya <prayingrun1001@gmail.com>
2019-10-18 21:26:01 +0900

    ch12

--- sousa.py
+++ sousa.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import datetime
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
